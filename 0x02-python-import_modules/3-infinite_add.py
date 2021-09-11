8b20070636c67c38beebdf79a33fd3a96308062e
topister onyango <topisternandera@gmail.com>
2021-09-11 04:38:09 -0700

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
-    total = 0
-    argc = len(sys.argv)
-    for i in range(1, argc):
-        total += int(sys.argv[i])
-    print(total)
+    i = 0
+    sum = 0
+    for i in range(len(sys.argv) - 1):
+        sum += int(sys.argv[i + 1])
+    print('{:d}'.format(sum))
