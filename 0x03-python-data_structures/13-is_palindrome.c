be5562ac8f75b3aa140a1129406b941865ac212d
topister onyango <topisternandera@gmail.com>
2021-09-13 01:11:48 -0700

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,37 +1,22 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * is_palindrome - Check is the listint is a Palindrome
- * @head: type listint_s double pointer of node
- * return: 1 if is a pilndrome 0 if not
+ * is_palindrome - Checks if a number is a palindrome
+ * @n: The given unsigned integer
+ *
+ * Return: Either 1 if n is a palindrome or 0 if not
  */
-
 int is_palindrome(listint_t **head)
 {
-	if (head == NULL && *head == NULL)
+	int remain, num, sum = 0;
+
+	for (num = n; n != 0; n = n / 10)
+	{
+		remain = n % 10;
+		sum = sum * 10 + remain;
+	}
+	if (num == sum)
 		return (1);
 	else
-		return (rec_palindrome(head, *head));
-}
-
-/**
- * rec_palindrome - Recursion to check each node as palindrome
- * @head: type listint_s double pointer of node
- * @tail: type listint_s single pointer of last node
- * return: 1 if is a pilndrome 0 if not
- */
-
-int rec_palindrome(listint_t **head, listint_t *tail)
-{
-	if (tail == NULL)
-		return (1);
-
-	if (rec_palindrome(head, tail->next) && (*head)->n == tail->n)
-	{
-		*head = (*head)->next;
-		return (1);
-	}
-	return (0);
+		return (0);
 }
