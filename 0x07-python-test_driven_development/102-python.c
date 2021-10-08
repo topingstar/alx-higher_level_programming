9125ff71344e530ffcf3b89acbdb2a4de4b98dc6
topister onyango <topisternandera@gmail.com>
2021-10-08 07:16:28 -0700

    python

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,23 +1,4 @@
-long int length;
-
-fflush(stdout);
-
-printf("[.] string object info\n");
-if (strcmp(p->ob_type->tp_name, "str") != 0)
-  {
-    printf("  [ERROR] Invalid String Object\n");
-    return;
-  }
-
-length = ((PyASCIIObject *)(p))->length;
-
-if (PyUnicode_IS_COMPACT_ASCII(p))
-  printf("  type: compact ascii\n");
- else
-   printf("  type: compact unicode object\n");
-printf("  length: %ld\n", length);
-printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
-}~#include <Python.h>
+#include <Python.h>
 #include <stdio.h>
 
 /**
