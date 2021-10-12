31d02ee3b5f701d5841895c4c7af3433250552b2
topister onyango <topisternandera@gmail.com>
2021-10-12 11:36:34 -0700

    Pascal's Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Pascal's Triangle"""
+''' class Student that defines a student
+'''
 
 
-def pascal_triangle(n):
-    """returns a list of lists of integers
-    representing the Pascal’s triangle of n"""
-    triangle = []
-    if n <= 0:
-        return []
-    for i in range(n):
-        a = 11 ** i
-        row = [int(digit) for digit in str(a)]
-        triangle += [row]
-    return triangle
+class Student:
+    '''module class student
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        '''method __init__
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
