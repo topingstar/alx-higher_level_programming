848f8894568af7aef6acf64e0ef9c04733297fba
topisteronyango <t.onyango@alustudent.com>
2021-12-13 07:01:52 -0800

    object fct

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -14,4 +14,20 @@ console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
+console.log(myObject);#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
 console.log(myObject);
