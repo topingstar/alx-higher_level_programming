1516a20893d809e233e61aa3bd0382ede53e9e2f
topisteronyango <t.onyango@alustudent.com>
2021-12-19 12:21:04 -0800

    model state

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-# List all State objects that contain letter 'a' from db 'hbtn_0e_6_usa'
-# Script should take 3 args: username, pw, and db name
-# Must use SQLAlchemy
-import sys
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+"""
+All states via SQLAlchemy
+"""
+from sys import argv
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    for instance in session.query(State).filter(State.name.like('%a%')):
-        print("{:d}: {}".format(instance.id, instance.name))
+    session = Session(engine)
+    data = session.query(State).order_by(State.id)\
+                               .filter(State.name.like("%a%")).all()
+    for row in data:
+        print("{}: {}".format(row.id, row.name))
+    session.close()
