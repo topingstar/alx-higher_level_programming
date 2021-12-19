677a0e4dbe3ee365606fe6960fd6a9e4839f45eb
topisteronyango <t.onyango@alustudent.com>
2021-12-19 12:58:30 -0800

    filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""displays all values in the states table of hbtn_0e_0_usa
-   where name matches matches an argument passed as a parameter"""
+
 
 import MySQLdb
 from sys import argv
 
+'''
+Script that lists all states from the database
+'''
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = '{}'".format(argv[4]))
-    for data in cursor.fetchall():
-        print(data)
-    cursor.close()
-    db.close()
+    cont = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1],
+        password=argv[2], database=argv[3])
+    cursor = cont.cursor()
+    cursor.execute(
+            "SELECT * FROM states WHERE name LIKE"
+            " '{:s}' ORDER BY id ASC".format(argv[4]))
+    db = cursor.fetchall()
+    for i in db:
+        if i[1] == argv[4]:
+            print(i)
