d6a2d35066f34e3fb24eceeec7353a882e435cde
topisteronyango <t.onyango@alustudent.com>
2022-01-08 07:33:10 -0800

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status"""
-import urllib.request
+# Python script that fetches a url
+import urllib.request as ur
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+with ur.urlopen('https://intranet.hbtn.io/status') as res:
+    res = res.read()
+
+print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(res), res))
+print('\t- utf8 content: {}'.format(str(res, 'utf-8')))
