79c9fa41c91c8c6cf7781092ee6b8c1b6b820084
topisteronyango <t.onyango@alustudent.com>
2022-01-08 06:19:43 -0800

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Function to find the peak of a list of integers
+"""
+  Function to find the peak of a list of integers
 """
 
 
 def find_peak(list_of_integers):
-    """returns the peak in a list of integers
+    """
+      returns the peak in a list of integers
     """
     if len(list_of_integers) != 0:
         return max(list_of_integers)
     else:
         return None
-    
