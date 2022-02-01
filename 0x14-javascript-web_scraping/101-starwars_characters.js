9ff07a025882b3306ad03149e2b41a0d2cb178ee
topisteronyango <t.onyango@alustudent.com>
2022-02-01 11:14:53 -0800

    starwars characters

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,29 +1,25 @@
 #!/usr/bin/node
-/* prints all characters of a Star Wars movie.
-    - the first argument is the Movie ID.
-    - displays one character name by line.
-*/
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (error) console.log(error);
-  else {
-    const characters = JSON.parse(body).characters;
-    characters_dict = {};
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (error) console.log(error);
-        else {
-          let id = JSON.parse(body).url;
-          id = id.replace('https://swapi-api.hbtn.io/api/people/', '');
-          id = id.replace('/', '');
-          const name = JSON.parse(body).name;
-          characters_dict[id] = (name);
-        }
-      });
-    });
-    for (let id in characters_dict) {
-        console.log(characters_dict[id]);
+
+function printCharacters (characters, idx) {
+  request(characters[idx], (err, res, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.parse(body).name);
+      if (idx + 1 < characters.length) {
+        printCharacters(characters, idx + 1);
+      }
     }
+  });
+}
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
